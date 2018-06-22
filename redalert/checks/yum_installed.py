961ae283359692f4354d7db82235cd7a34ad09cd
Mathew Robinson <chasinglogic@gmail.com>
2018-06-22 13:37:57 -0400

    Make code runnable

--- redalert/checks/yum_installed.py
+++ redalert/checks/yum_installed.py
@@ -2,4 +2,11 @@
 
 
 class YumCheck:
-    """Check if a package is installed via yum"""
+    """Check if a package is installed via yum
+
+    Args:
+        package: String name of the package to check
+    """
+
+    def __init__(self, package):
+        pass
